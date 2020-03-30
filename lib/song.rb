4f2b62c62c36654dbefedf465a00e07bd4bcbbbd
Mariam Haider <mfh.haider@gmail.com>
2020-03-30 21:29:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,12 @@
-class Song
-  attr_accessor :name, :artists, :genres
+class Song 
   @@count = 0
- 
-  def self.count
+  def self.count 
     @@count
   end 
   
+  @@genres = []
   def self.genres
-    @@genres = []
-  end
-  
-  def self.artists
-    @@artists = []
-  end 
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @@artists << artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
+    @@genres.uniq
   end
   
   def self.genre_count
@@ -35,8 +21,22 @@ class Song
     genre_count
   end
   
-   def self.artist_count
+  @@artists = []
+  def self.artist_count
     @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end
   
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  attr_accessor :name, :artist, :genre
 end 
